f0defb05114831d0acd1ef2ec8d630d6cf343302
Steon <lopezwil220697@gmail.com>
2025-04-29 14:42:42 -0600

    fd

--- SistemaMedico/Models/LoginDataModel.Designer.cs
+++ SistemaMedico/Models/LoginDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\wilmer\sismedico_edit_atleta\sismedico\SistemaMedico\SistemaMedico\Models\LoginDataModel.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\lopez\Source\Repos\SistemaMedico\SistemaMedico\Models\LoginDataModel.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
